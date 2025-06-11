3673cb0cb1cf73e7d1a5c274abb8b569a3c818a3
meme844 <xiurenyueyun@gmail.com>
2025-06-11 12:14:18 +0900

    計算
    console.logを使った計算と文字の出力

---
+++ First.js
@@ -0,0 +1,10 @@
+console.log("Hello world!");
+console.log("This is my first Javascript");
+// 5と3を足した値を出力してください
+console.log(5+3);
+
+// 20から8を引いた値を出力してください
+console.log(20-8);
+
+// "4 + 5" を文字列として出力してください
+console.log("4+5");
