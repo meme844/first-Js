37315f9b396a0facd210f59a879dd8ebf9e1538e
meme844 <xiurenyueyun@gmail.com>
2025-06-11 14:33:18 +0900

    変数
    letを使った変数定義のプログラム

---
+++ variable.js
@@ -0,0 +1,38 @@
+// 変数nameを定義し、「にんじゃわんこ」を代入してください
+let name="にんじゃわんこ";
+
+// 変数nameの値を出力してください
+console.log (name);
+
+// 変数lengthを定義してください
+let length=5;
+// 変数lengthの値を出力してください
+console.log (length);
+
+// 変数lengthを用いて、円の面積を出力してください
+console.log (length*length*3);
+
+let name = "にんじゃわんこ";
+console.log(name);
+
+// 変数nameの値を"とりずきん"に更新してください
+name ="とりずきん";
+
+// 変数nameの値を出力してください
+console.log(name);
+
+let number = 7;
+console.log(number);
+
+// 変数numberの値に3を加えてください
+number+=3;
+
+console.log(number);
+
+// 変数numberの値を2で割ってください
+number/=2;
+
+console.log(number);
+
+// ";"をわすれない！
+//number=number+3でもいいが、省略すると+=にできる
