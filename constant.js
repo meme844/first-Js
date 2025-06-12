b97e214f412341e5e1b999834463fde77de3ffec
meme844 <xiurenyueyun@gmail.com>
2025-06-12 10:16:28 +0900

    Create constant.js

---
+++ constant.js
@@ -0,0 +1,12 @@
+//定数languageを定義
+const language = "フランス語";
+//定数languageの値を出力
+console.log(language);
+
+const name = "にんじゃわんこ";
+const age = 14;
+
+//僕の名前は〇〇ですと出力
+console.log (`僕の名前は${name}です`);
+//今は〇〇歳ですと出力
+console.log (`今は${age}歳です`);
