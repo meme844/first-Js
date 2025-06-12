d364666cc6ba3c080475be7789e3f7023461f96d
meme844 <xiurenyueyun@gmail.com>
2025-06-12 13:50:20 +0900

    Create let.js

---
+++ let.js
@@ -0,0 +1,11 @@
+//1から5を出力するプログラム
+//変数numberの定義
+let number =0;
+//変数numberに1を加えて、そのあとに変数numberの値を出力
+number +=1;
+console.log(number);
+number +=1;
+console.log (number);number +=1;
+console.log (number);number +=1;
+console.log (number);number +=1;
+console.log (number);
