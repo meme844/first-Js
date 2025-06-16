739371673baeb089cd606884f6c358a0eb5c57bc
meme844 <xiurenyueyun@gmail.com>
2025-06-16 12:09:28 +0900

    Update let.js

--- let.js
+++ let.js
@@ -9,3 +9,12 @@ console.log (number);number +=1;
 console.log (number);number +=1;
 console.log (number);number +=1;
 console.log (number);
+//1から100まで表示するwhile文
+
+//変数numberを定義
+let number = 1;
+//while文
+while(number <=100){
+    console.log (number);
+    number +=1;
+}
