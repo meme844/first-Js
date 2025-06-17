f68863b060faa884a01ac5eb9139e7bc79f0e0dd
meme844 <xiurenyueyun@gmail.com>
2025-06-17 15:21:54 +0900

    Update if.js

--- if.js
+++ if.js
@@ -5,9 +5,19 @@ if (number == 1){ //numberが1の時
 }else{ //1以外の時
     console.log("きらい！");
 }
-//順位が1位だったら「おめでとう！」と表示する
+//順位が1位だったら「おめでとう！」と表示する,それ以外ならあと少し
 var number = parseInt(Math.random()*3)+1;
 console.log("あなたの順位は"+number+"位です");
 if (number == 1){
     console.log("おめでとう！");
+}else{
+    console.log("あと少し");
+}
+//100～300の数値を100刻みでランダムに生成して、ポイントとして表示するプログラムです
+var number = (parseInt(Math.random()*3)+1)*100;
+console.log("あなたの得点は"+number+"ポイントです");
+if (number == 300){
+    console.log("おめでとう");
+}else{
+    console.log("残念");
 }
