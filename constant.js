4e7d41e8b9034be0e683a8360743f6d6ad2e35d0
meme844 <xiurenyueyun@gmail.com>
2025-06-17 09:20:34 +0900

    Update constant.js

--- constant.js
+++ constant.js
@@ -10,3 +10,11 @@ const age = 14;
 console.log (`僕の名前は${name}です`);
 //今は〇〇歳ですと出力
 console.log (`今は${age}歳です`);
+
+//varを使った変数
+var player = "勇者"; //ここを変えるとレベルアップした！の前の部分が変わる
+console.log(player + "はレベルアップした！");
+//数の表示とサイコロ
+//var,parseInt(Math.ramdom)
+var number = parseInt(Math.random()*6) +1;
+console.log("サイコロの目は" +number +"です");
